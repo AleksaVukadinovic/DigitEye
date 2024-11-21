50a383bdd6f11d902f59863668eb6ea9c8b1b7f3
Aleksa Vukadinović <alexvukadin8@gmail.com>
2024-11-21 22:59:17 +0100

    Pocetak, keras mnogo zajebava

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import cv2 as cv
+import  matplotlib.pyplot as plt
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(X_train, Y_train), (X_test, Y_test) =mnist.load_data()
+
+X_train = tf.keras.utils.normalize(X_train, axis=1)
+X_test = tf.keras.utils.normalize(X_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
