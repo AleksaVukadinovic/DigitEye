65e30f7c586926c878de8ae72f679c9d612952fc
Aleksa Vukadinović <alexvukadin8@gmail.com>
2024-11-28 01:10:43 +0100

    Glavni deo gotov

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DigitEye
-A Python and TensorFlow-powered neural network for recognizing handwritten digits with precision.
+A Python and TensorFlow-powered convolutional neural network for recognizing handwritten digits with (relative) precision.
+
+
+## Description
+
+This project demonstrates the use of deep learning to recognize handwritten digits. The application is built with TensorFlow and supports model training, evaluation, and digit prediction. It can be run from the command line, with options to train a model or use an existing one to predict digits from images.
+
+## Dependencies
+
+- **Operating System**: Windows 10/11, macOS, or Linux
+- **Python Version**: 3.8 or later
+- Libraries required:
+  - TensorFlow
+  - NumPy
+  - OpenCV
+  - Matplotlib
+
--- main.py
+++ main.py
@@ -1,13 +1,54 @@
 import os
 import numpy as np
-import cv2 as cv
+import cv2
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-mnist = tf.keras.datasets.mnist
-(X_train, Y_train), (X_test, Y_test) =mnist.load_data()
+# Change this if you don't want to train a new model each time you run this code
+train_new_model = True
 
-X_train = tf.keras.utils.normalize(X_train, axis=1)
-X_test = tf.keras.utils.normalize(X_test, axis=1)
+if train_new_model:
+    # Loading the MNIST data set with samples and splitting it
+    mnist = tf.keras.datasets.mnist
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    # Normalizing the data 
+    X_train = tf.keras.utils.normalize(X_train, axis=1)
+    X_test = tf.keras.utils.normalize(X_test, axis=1)
+
+    # Create a neural network 
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+    # Compiling and optimizing model
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Training the model
+    model.fit(X_train, y_train, epochs=3)
+
+    # Evaluating the model (optional)
+    val_loss, val_acc = model.evaluate(X_test, y_test)
+    print(val_loss)
+    print(val_acc)
+
+    model.save('handwritten_digits.keras')
+else:
+    model = tf.keras.models.load_model('handwritten_digits.keras')
+
+# Load custom images and predict them
+image_number = 1
+while os.path.isfile('digits/digit{}.png'.format(image_number)):
+    try:
+        img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print("The number is probably a {}".format(np.argmax(prediction)))
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+        image_number += 1
+    except:
+        print("Error reading image! Proceeding with next image...")
+        image_number += 1
